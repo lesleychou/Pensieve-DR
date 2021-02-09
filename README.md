e917075dfc6a6bc52db3c0079527bf83e8cfa756
Kasim Te <kasimte@gmail.com>
2021-02-09 22:29:56 +0900

    Get sim/bo.py running.

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 ```
 conda create -n pensieve-dr python=3.7
 conda activate pensieve-dr
-pip install tensorflow==1.14.0 visdom numpy tflearn
+pip install tensorflow==1.14.0 visdom numpy tflearn numba bayesian-optimization scikit-learn==0.22
 pip list
 ```
 
@@ -29,5 +29,6 @@ Example:
 cd sim
 python rl_test.py \
        --summary_dir "../MPC_RL_test_results/" \
-       --model_path "../new-DR-results/sanity-check-3/model_saved/nn_model_ep_5200.ckpt"        
+       --model_path "../data/sanity-check-3/model_saved/nn_model_ep_5200.ckpt" \
+       --test_trace_dir "../data/example_traces"
 ```
--- sim/bo.py
+++ sim/bo.py
@@ -19,9 +19,9 @@ def black_box_function(x):
     '''
     # TODO: this need to be args.summary_dir
     # TODO: do i need to load the actor_path here?
-    path = './new-DR-results/sanity-check-3/model_saved'
+    # path = './new-DR-results/sanity-check-3/model_saved'
     #actor_path = latest_actor_from(path)
-    latest_model_path = "../new-DR-results/sanity-check-3/model_saved/nn_model_ep_5200.ckpt"
+    latest_model_path = "../data/sanity-check-3/model_saved/nn_model_ep_5200.ckpt"
 
     # --test-trace-dir='../pensieve/data/generated_traces_ts_float/train/123/' \
     #   --video-size-file-dir='../pensieve/data/video_size_6_larger/' \
@@ -29,12 +29,13 @@ def black_box_function(x):
 
     command = " python rl_test.py  \
                 --CURRENT_PARAM={current_max_tp_param} \
-                --test_trace_dir = '../data/example_traces/' \
-                --summary_dir = '../MPC_RL_test_results/' \
-                --model_path = '{model_path}'" \
-                .format(current_max_tp_param=x, model_path=latest_model_path)
+                --test_trace_dir='../data/example_traces/' \
+                --summary_dir='../MPC_RL_test_results/' \
+                --model_path='{model_path}' \
+                ".format(current_max_tp_param=x, model_path=latest_model_path)
 
     r = float(subprocess.check_output(command, shell=True, text=True).strip())
+    print(r)
     return r
 
 pbounds = {'x': (0 ,100)}
@@ -45,10 +46,10 @@ optimizer = BayesianOptimization(
     )
 
 optimizer.maximize(
-    init_points=1 ,
-    n_iter=2 ,
-    kappa=10,
-    xi=0.1
+    init_points=1,
+    n_iter=2
+    # kappa=10,
+    # xi=0.1
 )
 next = optimizer.max
 param = next.get( 'params' ).get( 'x' )
